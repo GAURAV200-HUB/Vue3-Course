d58c390b14658783ab351e71c85ff61d98254a99
GAURAV200-HUB <78646815+GAURAV200-HUB@users.noreply.github.com>
2021-08-17 17:08:44 +0530

    Add files via upload

---
+++ dojo-blog/src/composables/getPosts.js
@@ -0,0 +1,31 @@
+// import { resolve } from 'core-js/fn/promise'
+// import { Promise } from 'core-js/fn/promise'
+import { ref } from 'vue'
+
+const getPosts = () => {
+    const posts = ref([])
+    const error = ref(null)
+
+    const load = async () => {
+      try {
+      //   // simulate delay
+      //   await new Promise(resolve => {
+      //     setTimeout(resolve, 2000)
+      // })
+
+        let data = await fetch('http://localhost:3000/posts')
+        if(!data.ok) {
+          throw Error('no available data')
+        }
+        posts.value = await data.json()
+      }
+      catch(err) {
+        error.value = err.message
+        console.log(error.value)
+      }
+    }
+
+    return { posts, error, load }
+}
+
+export default getPosts
